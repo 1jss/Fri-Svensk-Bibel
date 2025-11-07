3354745a4cc9c414f0aabf48fad72d7d351a615e
1jss <62594271+1jss@users.noreply.github.com>
2025-11-07 08:03:58 +0100

    4800

--- workflow.js
+++ workflow.js
@@ -8,16 +8,16 @@ const rl = readline.createInterface({
 });
 
 function checkReplacements() {
-  rl.question('Are you content with the replacements? (y/n): ', (answer) => {
+  rl.question('>>> Are you content with the replacements? (y/n): ', (answer) => {
     if (answer.toLowerCase() === 'y') {
       // Wait for user committing changes
-      rl.question('Commit your changes and press Enter when done: ', () => {
+      rl.question('>>> Commit your changes and press Enter when done: ', () => {
         // Loop back to new line number input
         askLineNumber();
       });
     } else {
       // Wait for user to edit replacements.json and revert changes
-      rl.question('Edit replacements.json and revert changes if needed, then press Enter: ', () => {
+      rl.question('>>> Edit replacements.json and revert changes if needed, then press Enter: ', () => {
         // Run replace.js again
         execSync('node replace.js', { stdio: 'inherit' });
         // Check again
@@ -28,7 +28,7 @@ function checkReplacements() {
 }
 
 function askLineNumber() {
-  rl.question('Enter line number: ', (lineNumber) => {
+  rl.question('>>> Enter line number: ', (lineNumber) => {
     // Copy FSB.xml to FSB_pre.xml
     fs.copyFileSync('FSB.xml', 'FSB_pre.xml');
     
@@ -36,17 +36,17 @@ function askLineNumber() {
     execSync(`node create_context.js ${lineNumber} 25`, { stdio: 'inherit' });
     
     // Wait for user edits in temp_context.xml
-    rl.question('Edit temp_context.xml and press Enter when done: ', () => {
+    rl.question('>>> Edit temp_context.xml and press Enter when done: ', () => {
       // Run merge_context.js with line number
       execSync(`node merge_context.js ${lineNumber} 25`, { stdio: 'inherit' });
       
       // Wait for user committing changes
-      rl.question('Commit your changes and press Enter when done: ', () => {
+      rl.question('>>> Commit your changes and press Enter when done: ', () => {
         // Run diff_finder.js
         execSync('node diff_finder.js', { stdio: 'inherit' });
         
         // Wait for user reviewing replacements.json
-        rl.question('Review replacements.json and press Enter when done: ', () => {
+        rl.question('>>> Review replacements.json and press Enter when done: ', () => {
           // Run replace.js
           execSync('node replace.js', { stdio: 'inherit' });
           
