ca8ce362db43746c8e8605c60b1c40d7ff8edf95
1jss <62594271+1jss@users.noreply.github.com>
2026-01-19 07:43:57 +0100

    Manually checked replacements

--- workflows/replacement_apply/apply_replacements.js
+++ workflows/replacement_apply/apply_replacements.js
@@ -1,14 +1,22 @@
 const fs = require('fs');
 const config = require('../../config.js');
 
-const replacements = JSON.parse(fs.readFileSync(config.data.changes.replacements, 'utf8'));
+const replacements = JSON.parse(fs.readFileSync(config.data.changes.replacementsUnchecked, 'utf8'));
 
 let xmlContent = fs.readFileSync(config.data.bibles.fsbXml, 'utf8');
 
+const notApplied = [];
+
 for (const rep of replacements) {
-    xmlContent = xmlContent.split(rep.old).join(rep.new);
+    if (xmlContent.includes(rep.old)) {
+        xmlContent = xmlContent.split(rep.old).join(rep.new);
+    } else {
+        notApplied.push(rep);
+    }
 }
 
 fs.writeFileSync(config.data.bibles.fsbXml, xmlContent);
+fs.writeFileSync(config.data.changes.replacementsUnchecked, JSON.stringify(notApplied, null, 2));
 
-console.log('Replacements applied successfully.');
+console.log(`Applied ${replacements.length - notApplied.length} replacements.`);
+console.log(`${notApplied.length} replacements had no match and were kept in the file.`);
